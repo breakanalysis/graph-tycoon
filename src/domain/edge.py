d30012c7e1fd72209147dbe9e4fc8c7dc7dd305c
Jacob Sznajdman <breakanalysis@gmail.com>
2018-02-15 21:46:13 +0100

    added edge margin function and entrace queue also some small fixes

--- src/domain/edge.py
+++ src/domain/edge.py
@@ -1,4 +1,5 @@
 import math
+from collections import deque
 
 class Edge:
     """Graph edge in the plane representing destinations and road crossings.
@@ -8,13 +9,18 @@ class Edge:
         end (Node): destination node of the edge.
         speed (float): speed limit of edge.
         cars (set): set of cars on the edge. 
+        length (float): the length of the edge.
+        queue (deque): cars queued for entering this edge.
     """
     def __init__(self, start, end, speed):
         self.start = start
         self.end = end
         self.speed = speed
-        self.cars = set()
+        self.cars = []
         self.length = math.sqrt((start.x - end.x)**2 + (start.y - end.y)**2)
+        self.queue = deque([])
 
-        
+    def entrance_margin(self, car_length):
+        last_car = self.cars[-1]
+        return last_car.dist - car_length
         
