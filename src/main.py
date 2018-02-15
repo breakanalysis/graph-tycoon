9a7de886fbbd8ea1ef5c8070ab0f8c9522172637
Jacob Sznajdman <breakanalysis@gmail.com>
2018-02-15 21:47:32 +0100

    some exploratory code in main.py

--- src/main.py
+++ src/main.py
@@ -7,4 +7,8 @@ hello.add_edge("1", "2", 1.0)
 node1 = hello.nodes["1"]
 edge = node1.outs["2"]
 hello.add_car(edge)
+edge.queue.append(1)
+edge.queue.append(2)
+edge.queue.append(3)
+print(edge.queue.popleft())
 
