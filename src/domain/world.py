00fc071744e269b3ebf7c14e740a3346a30c8e0e
Jacob Sznajdman <breakanalysis@gmail.com>
2018-02-17 17:16:36 +0100

    added to_string to world and worked out a subset of bugs

--- src/domain/world.py
+++ src/domain/world.py
@@ -41,13 +41,19 @@ class World:
         self.cars.add(car)
     
     def entrance_free(self, edge):
+        if len(edge.cars) == 0:
+            return True
         last_car = edge.cars[-1]
         return last_car.dist > 1.5*car_length
+    def get_edge(self, start, end):
+        node = self.nodes[start]
+        return node.outs[end]
 
     def step(self, time, decisions):
+#        import pdb; pdb.set_trace()
         for edge in self.edges:
             if len(edge.queue) > 0 and self.entrance_free(edge):
-                entering = edge.queue.popright()
+                entering = edge.queue.pop()
                 entering.state = 'exiting'
                 entering.edge = edge
                 entering_dist = 0
@@ -55,14 +61,16 @@ class World:
             new_car_distance_pairs = deque([])
             next_car = None
             next_dist = 99999999.0
-            for car, dist in reversed(edge.queue):
+            for car, dist in reversed(edge.cars):
                 new_dist = car.edge.speed * time + dist
                 new_dist = max(new_dist, next_dist - 1.5*self.car_length)
                 if (car.state == 'moving'):
                     if (new_dist >= edge.length):
                         new_dist = edge.length
                         car.state = 'queued'
-                        transit_edge = decisions[car]
+                        transit_edge = edge.end.outs.values()[0]
+                        if (car in decisions):
+                            transit_edge = decisions[car]
                         transit_edge.queue.appendleft(car)
                     new_car_distance_pairs.appendleft((car, new_dist))
                 elif (car.state == 'exiting'):
@@ -72,6 +80,15 @@ class World:
                     new_car_distance_pairs.appendleft((car, dist))
                 else:
                     raise Exception('unknown state: ' + car.state)
-            edge.queue = new_car_distance_pairs
+            edge.cars = new_car_distance_pairs
+
+    def to_string(self):
+        s = ""
+        for start_name, start_node in self.nodes.iteritems():
+            for end_name, edge in start_node.outs.iteritems():
+                car_dists = ", ".join(map(lambda x: x[0].state+":"+str(x[1]), edge.cars))
+                s += start_name + " --> (" + car_dists + ") --> " + end_name + '\n'
+        return s
+
 
 
--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
 from domain.world import *
 
-hello = World(1.0)
+hello = World(0.05)
 hello.add_node("1", 0.0,0.0)
-hello.add_node("2", 1.0,1.0)
-hello.add_edge("1", "2", 1.0)
-node1 = hello.nodes["1"]
-edge = node1.outs["2"]
-hello.add_car(edge)
-edge.queue.append(1)
-edge.queue.append(2)
-edge.queue.append(3)
-print(edge.queue.popleft())
+hello.add_node("2", 1.0,0.0)
+hello.add_node("3", 2.0,0.0)
+hello.add_node("4", 3.0,0.0)
+hello.add_node("5", 4.0,0.0)
+
+hello.add_edge("1", "2", 0.05)
+hello.add_edge("2", "3", 0.05)
+hello.add_edge("3", "4", 0.05)
+hello.add_edge("4", "5", 0.05)
+
+hello.add_car(hello.get_edge("1","2"), 'moving')
+
+hello.step(1.0, {})
+
+print(hello.to_string())
 
