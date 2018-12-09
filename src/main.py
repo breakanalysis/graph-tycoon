8f0e722ab046625062be367256bbb9cdac8b8ea8
Jacob Sznajdman <breakanalysis@gmail.com>
2018-12-09 21:27:05 +0100

    some cleanup

--- src/main.py
+++ src/main.py
@@ -9,42 +9,7 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 pink = (255, 200, 200)
 
-hello = World()
-hello.add_node("1", 0.1, 0.3)
-hello.add_node("2", 0.4, 0.1)
-hello.add_node("3", 0.8, 0.4)
-hello.add_node("4", 0.6, 0.8)
-hello.add_node("5", 0.3, 0.4)
-hello.add_node("6", 0.2, 0.7)
-
-hello.add_edge("1", "2", 0.01)
-hello.add_edge("2", "3", 0.01)
-hello.add_edge("3", "4", 0.01)
-hello.add_edge("4", "5", 0.01)
-hello.add_edge("5", "1", 0.01)
-hello.add_edge("1", "4", 0.01)
-hello.add_edge("2", "5", 0.01)
-hello.add_edge("4", "6", 0.01)
-hello.add_edge("6", "1", 0.01)
-
-hello.add_car(hello.get_edge("1", "2"), 'moving')
-hello.add_car(hello.get_edge("1", "2"), 'moving')
-hello.add_car(hello.get_edge("5", "1"), 'moving')
-hello.add_car(hello.get_edge("5", "1"), 'moving')
-hello.add_car(hello.get_edge("3", "4"), 'moving')
-hello.add_car(hello.get_edge("3", "4"), 'moving')
-hello.add_car(hello.get_edge("1", "4"), 'moving')
-hello.add_car(hello.get_edge("2", "5"), 'moving')
-hello.add_car(hello.get_edge("2", "5"), 'moving')
-hello.add_car(hello.get_edge("4", "5"), 'moving')
-hello.add_car(hello.get_edge("6", "1"), 'moving')
-
 hello = generate_world()
-# steps = 0
-# while True:
-#     hello.step(1.0, {})
-#     steps += 1
-#     print(hello)
 assert hello.validate_reachability()
 
 import pygame
@@ -70,7 +35,7 @@ while not done:
             ex = SCALE * edge.end.x
             dy = ey - sy
             dx = ex - sx
-            L = edge.length * SCALE 
+            L = edge.length * SCALE
             dy = dy / L
             dx = dx / L
             pygame.draw.line(screen, pink, (sy, sx), (ey, ex), int(SCALE/100))
@@ -85,4 +50,3 @@ while not done:
                 pygame.draw.line(screen, green, car_start, car_end, int(SCALE/30))
     clock.tick(20)
     pygame.display.flip()
-
